eecc03331080cfaf9b59d6f7e873a3d2b395ae7b
michaelhesch <michael.hesch@gmail.com>
2021-05-19 15:48:39 +0100

    add jQuery & javascript code

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    
+    $("h2").addClass("underline"); //adds underline classs to all h2 elements once DOM has loaded
+    $("ul").addClass("border"); //adds border class to all list elements once DOM has loaded
 });
